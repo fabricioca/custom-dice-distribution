bc77be7973a7f4f936545bf9d5bf0c2b2a37fe2f
Fabricio Caixeta Andriani <fcandriani@gmail.com>
2020-01-25 14:00:19 -0300

    divide methods and add lower and upper prob

--- CustomDiceDistribution.py
+++ CustomDiceDistribution.py
@@ -5,24 +5,30 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
 import random
+import math
 
-def diceProb(dices, sybols = [], title = "", n = 10000):
+def getSymbols(dices):
+    symbols = {}
+    for dice in dices:
+        for face in dice:
+            for value in face:
+                symbols[value] = True
+    symbols = list(symbols.keys())
 
-    if(len(sybols) == 0):
-        sybols = {}
-        for dice in dices:
-            for face in dice:
-                for value in face:
-                    sybols[value] = True
-        sybols = list(sybols.keys())
+    return symbols
+
+def diceRolls(dices, symbols = [], n = 10000):
+
+    if(len(symbols) == 0):
+        symbols = getSymbols(dices)
 
     rollResults = {}
-    for i in sybols:
+    for i in symbols:
         rollResults[i] = []
 
     for _ in range(n):
         rollResult = {}
-        for i in sybols:
+        for i in symbols:
             rollResult[i] = 0
         
         for dice in dices:
@@ -30,27 +36,82 @@ def diceProb(dices, sybols = [], title = "", n = 10000):
             for i in value:
                 rollResult[i] += 1 
 
-        for i in sybols:
+        for i in symbols:
             rollResults[i].append(rollResult[i])
 
+    return rollResults
 
-    fig,ax =  plt.subplots(1,len(sybols), squeeze=False, figsize=(3*len(sybols),5))
-    fig.suptitle(title)
+def diceProb(rolls):   
+    prob = {}
 
-    fig.tight_layout()
+    for i in rolls.keys():
+        distribution = Counter(rolls[i])
 
-    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.5, hspace=None)
-
-    for i in range(len(sybols)):
-        distribution = Counter(rollResults[sybols[i]])
+        n = len(rolls[i])
 
         keys = distribution.keys()
         values = [x/n for _,x in sorted(zip(keys, distribution.values()))]   
         
         keys = sorted(keys)
-        keys = list(map(str, keys))
+
+        prob[i] = [keys,values]
+
+    return prob
+
+def diceProbUpper(rolls):
+    prob = diceProb(rolls)
+
+    for i in list(prob.keys()):
+        values = []
+
+        for j in range(len(prob[i][0])):
+            sum = 0
+            for k in range(len(prob[i][0])):
+                if (prob[i][0][k] >= prob[i][0][j]):
+                    sum += prob[i][1][k]
+            values.append(sum)
+
+        prob[i][1] = values
+
+    return prob
+
+def diceProbLower(rolls):
+    prob = diceProb(rolls)
+
+    for i in list(prob.keys()):
+        values = []
+
+        for j in range(len(prob[i][0])):
+            sum = 0
+            for k in range(len(prob[i][0])):
+                if (prob[i][0][k] <= prob[i][0][j]):
+                    sum += prob[i][1][k]
+            values.append(sum)
+
+        prob[i][1] = values
+
+    return prob
+
+def diceProbGraph(data, symbols = [], title = ""):
+
+    if (len(symbols) == 0):
+        symbols = list(data.keys())
+
+    fig,ax =  plt.subplots(1,len(symbols), squeeze=False, figsize=(3*len(symbols),5))
+    
+    if(title != ""):
+        fig.suptitle(title)
+
+    fig.tight_layout()
+
+    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.5, hspace=None)
+
+    for i in range(len(symbols)):
+        graphData = data[symbols[i]]
+
+        keys = list(map(str, graphData[0]))
         
-        ax[0][i].bar(keys, values)
-        ax[0][i].set_title('Amount of "' + sybols[i] + '"')
+        ax[0][i].bar(keys,graphData[1])
+        ax[0][i].set_title('Amount of "' + symbols[i] + '"')
 
     plt.show()
--- examples.py
+++ examples.py
@@ -1,17 +1,53 @@
 import CustomDiceDistribution as cdd
 
-# # Terror Dice
-sybols = ["x","-","s"]
+#TODOs
+# Exportar .csv com tabelas com os valores de probabilidade
+
+def copyDice(dice, number = 1):
+    dices = []
+    for _ in range(number):
+        dices.append(dice)
+
+    return dices
+
+def graphs(dices):
+    rolls = cdd.diceRolls(dices)
+
+    prob = cdd.diceProb(rolls)
+    cdd.diceProbGraph(prob)
+
+    probUpper = cdd.diceProbUpper(rolls)
+    cdd.diceProbGraph(probUpper)
+
+    probLower = cdd.diceProbLower(rolls)
+    cdd.diceProbGraph(probLower)
+
+def diceProb(dice, number = 1): 
+    dices = copyDice(dice, number)
+    graphs(dices)
+
+
+
+regularDice = [".","..","...","....",".....","......"]
 terrorDice = ["x","-","-","-","s","ss"]
-dices = [terrorDice, terrorDice, terrorDice, terrorDice, terrorDice]
-cdd.diceProb(dices, title="5 Terror Dice Probability")
+arkaranHorrorDice = ["-","-","-","-","s","s"]
+MansionsOfMadnessDice = ["-","-","-","c","c","s","s","s"]
 
-# # Regular Dice
-# regularDice = ["x","xx","xxx","xxxx","xxxxx","xxxxxx"]
-# dices = [regularDice, regularDice]
-#diceProb(dices, title="2 Regular Dice Sum")
+# Terror Dice 5
+diceProb(terrorDice, number = 5)
 
-# Arkahan Horror
-ahDice = ["-","-","-","-","s","s"]
-dices = [ahDice, ahDice]
-cdd.diceProb(dices, title="2 A.H. Dice")
+# Regular Dice 2
+# diceProb(regularDice, number = 2)
+
+# Regular Dice 3
+# diceProb(regularDice, number = 3)
+
+# Arkahan Horror 5
+# diceProb(arkaranHorrorDice, number = 5)
+
+# Mansions of Madness 3
+# diceProb(MansionsOfMadnessDice, number = 3)
+
+
+    
+    
